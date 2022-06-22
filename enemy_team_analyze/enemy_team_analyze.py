0fd61455a92b611005b3da7c1217c0b307bd26ad
ZeroRin <pptxp@qq.com>
2022-06-22 11:18:59 +0800

    Finish enemy team analyzer

--- enemy_team_analyze/enemy_team_analyze.py
+++ enemy_team_analyze/enemy_team_analyze.py
@@ -20,17 +20,25 @@ stc_data = get_stc_data(
 get_df = lambda d, k: pd.DataFrame.from_records(iter(d.values()),columns=k,index='id').convert_dtypes()
 s = get_df(stc_data['spot'],['id','mission_id','enemy_team_id'])
 m = get_df(stc_data['mission'],['id','name'])
-t = get_df(stc_data['enemy_team'],['id','enemy_leader'])
+t = get_df(stc_data['enemy_team'],['id','enemy_leader','effect_ext'])
 e = get_df(stc_data['enemy_in_team'],['id','enemy_team_id', 'enemy_character_type_id','number'])
 c = get_df(stc_data['enemy_character_type'],['id','name'])
 # %%
-ec = e.join(c,on='enemy_character_type_id',rsuffix='_enemy_character_type',how='left').drop(columns='enemy_character_type_id').groupby(['enemy_team_id','name']).sum()
+ec = e.join(c,on='enemy_character_type_id',rsuffix='_enemy_character_type',how='left').drop(columns='enemy_character_type_id').groupby(['enemy_team_id','name']).sum().reset_index()
+ec['member'] = ec.apply(lambda x: f"{x['name']}*{x['number']}",axis=1)
+ec = ec.groupby(['enemy_team_id']).agg({'member':'|'.join})
+ec
 # %%
 tl = t.join(c,on='enemy_leader').drop(columns='enemy_leader').rename(columns={'name':'leader'})
+m['name'] = m.apply(lambda x: re.sub('//n','',x['name']).split('|')[0],axis=1)
 ms = s.join(m,on='mission_id').drop(columns=['mission_id'])
-mstl = ms.join(tl,on='enemy_team_id',how='right').rename(columns={'name':'mission'}).groupby('enemy_team_id').first().fillna('')
+mstl = ms.join(tl,on='enemy_team_id',how='right').rename(columns={'name':'mission'}).groupby('enemy_team_id').first()
 mstl
 # %%
-mstlec = mstl.join(ec.reset_index().set_index('enemy_team_id')).reset_index().set_index(['enemy_team_id','leader','mission'])
+mstlec = mstl.join(ec,how='left').reindex(columns=['mission','leader','effect_ext','member'])
+mstlec = mstlec.fillna({'mission':'','leader':'','member':'','effect_ext':0,})
+mstlec['effect_ext'] = mstlec.apply(lambda x: str(x['effect_ext']) if x['effect_ext'] > 0 else '',axis=1)
 mstlec
+# %%
+mstlec.to_csv('enemy_team_info.csv')
 # %%
