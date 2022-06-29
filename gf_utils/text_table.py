68cd90cb927a3265bfa1527e6174f9e138f4f35c
ZeroRin <pptxp@qq.com>
2022-06-29 11:38:50 +0800

    modify text_table to suit us server's fking usage of comma in texttables

--- gf_utils/text_table.py
+++ gf_utils/text_table.py
@@ -2,6 +2,7 @@
 import os
 import csv
 import logging
+import re
 # %%
 class TextTable():
     def __init__(self, table_dir):
@@ -11,10 +12,11 @@ class TextTable():
                 continue
             logging.debug(f'Reading {fname}')
             with open(os.path.join(table_dir,fname),'r',encoding='utf-8') as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    # print(row)
-                    k,v = row
+                for line in f.readlines():
+                    k,*vs = line.split(',')
+                    v = ','.join(vs)
+                    v = re.sub(r'//c',',',v)
+                    v = re.sub(r'//n','\n',v)
                     self.dict[k] = v
     
     def __call__(self, k):
@@ -24,9 +26,11 @@ class TextTable():
 # %%
 if __name__=='__main__':
     logging.basicConfig(level='DEBUG',force=True)
-    table_dir = r'..\data-miner\data\ch\asset\table'
+    table_dir = r'..\data\us\asset\table'
     table = TextTable(table_dir)
-    print(table('achievement-20000015'),table(32))
+    print(table('battle_skill_config-210040301'))
+    print(table('battle_skill_config-211001802'))
+    print(table(1234567))
 
 
 # %%
