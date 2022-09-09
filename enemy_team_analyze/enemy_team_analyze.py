f0c71086e6df5ca8f48994f9c9e4b109153151ed
ZeroRin <pptxp@qq.com>
2022-09-10 07:34:47 +0800

    add enemy team ai

--- enemy_team_analyze/enemy_team_analyze.py
+++ enemy_team_analyze/enemy_team_analyze.py
@@ -13,16 +13,17 @@ region = 'ch'
 stc_data = get_stc_data(
     stc_dir=f'../data/{region}/stc', 
     table_dir=f'../data/{region}/asset/table',
-    subset=['enemy_character_type','enemy_in_team','enemy_team','spot','mission'],
+    subset=['enemy_character_type','enemy_in_team','enemy_team','spot','mission','team_ai'],
 )
 
 # %%
 get_df = lambda d, k: pd.DataFrame.from_records(iter(d.values()),columns=k,index='id').convert_dtypes()
 s = get_df(stc_data['spot'],['id','mission_id','enemy_team_id'])
 m = get_df(stc_data['mission'],['id','name'])
-t = get_df(stc_data['enemy_team'],['id','enemy_leader','effect_ext'])
+t = get_df(stc_data['enemy_team'],['id','enemy_leader','effect_ext','ai'])
 e = get_df(stc_data['enemy_in_team'],['id','enemy_team_id', 'enemy_character_type_id','number'])
 c = get_df(stc_data['enemy_character_type'],['id','name'])
+a = get_df(stc_data['team_ai'],['id','ai_type','name'])
 # %%
 ec = e.join(c,on='enemy_character_type_id',rsuffix='_enemy_character_type',how='left').drop(columns='enemy_character_type_id').groupby(['enemy_team_id','name']).sum().reset_index()
 ec['member'] = ec.apply(lambda x: f"{x['name']}*{x['number']}",axis=1)
@@ -30,9 +31,12 @@ ec = ec.groupby(['enemy_team_id']).agg({'member':'|'.join})
 ec
 # %%
 tl = t.join(c,on='enemy_leader').drop(columns='enemy_leader').rename(columns={'name':'leader'})
+tla = tl.merge(a,how='left',left_on='ai',right_on='ai_type').rename(columns={'name':'ai_name'})
+tla 
+# %%
 m['name'] = m.apply(lambda x: re.sub('//n','',x['name']).split('|')[0],axis=1)
 ms = s.join(m,on='mission_id').drop(columns=['mission_id'])
-mstl = ms.join(tl,on='enemy_team_id',how='right').rename(columns={'name':'mission'}).groupby('enemy_team_id').first()
+mstl = ms.join(tla,on='enemy_team_id',how='right').rename(columns={'name':'mission'}).groupby('enemy_team_id').first()
 mstl
 # %%
 mstlec = mstl.join(ec,how='left').reindex(columns=['mission','leader','effect_ext','member'])
