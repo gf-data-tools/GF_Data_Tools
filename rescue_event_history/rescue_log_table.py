7805a79f0cea5afab20a9882331d50cd466cdf06
ZeroRin <pptxp@qq.com>
2023-04-27 01:49:50 +0800

    winter? event

--- rescue_event_history/rescue_log_table.py
+++ rescue_event_history/rescue_log_table.py
@@ -8,78 +8,109 @@ import pandas as pd
 import re
 
 from pathlib import Path
+
 os.chdir(Path(__file__).resolve().parent)
 # %%
-region = 'ch'
+region = "ch"
 stc_data = get_stc_data(
-    stc_dir=f'../data/{region}/stc', 
-    table_dir=f'../data/{region}/asset/table',
-    subset=['gun'],
+    stc_dir=f"../data/{region}/stc",
+    table_dir=f"../data/{region}/asset/table",
+    subset=["gun"],
 )
 
-dstr2date = lambda dstr: datetime.date(2000+int(dstr[:2]),int(dstr[2:4]),int(dstr[4:]))
+dstr2date = lambda dstr: datetime.date(
+    2000 + int(dstr[:2]), int(dstr[2:4]), int(dstr[4:])
+)
 
 gun_info = dict()
-for gun in stc_data['gun'].values():
-    if gun['id']<2000:
-        gun_info[gun['name']]=dict(
-            name=gun['name'],
-            id=gun['id'],
-            rank=gun['rank'] if not gun['is_additional'] else 1,
+for gun in stc_data["gun"].values():
+    if gun["id"] < 2000:
+        gun_info[gun["name"]] = dict(
+            name=gun["name"],
+            id=gun["id"],
+            rank=gun["rank"] if not gun["is_additional"] else 1,
         )
 
 rescue_log = dict()
-with open('doll_obtain_info.tsv', encoding='utf-8') as f:
-    for row in csv.reader(f,delimiter='\t'):   
+with open("doll_obtain_info.tsv", encoding="utf-8") as f:
+    for row in csv.reader(f, delimiter="\t"):
         event, start_time, end_time, *dolls = row
-        start_time=dstr2date(start_time)
-        end_time=dstr2date(end_time)
+        start_time = dstr2date(start_time)
+        end_time = dstr2date(end_time)
         for name in dolls:
-            rescue_log.setdefault(name, {'first':{'event':'','time':dstr2date('300101')},'last':{'event':'','time':dstr2date('160520')},'info':gun_info[name]})
-            if start_time <= rescue_log[name]['first']['time']:
-                rescue_log[name]['first'] = {'event':event,'time':start_time}
-            if end_time >= rescue_log[name]['last']['time']:
-                rescue_log[name]['last'] = {'event':event,'time':end_time}
+            rescue_log.setdefault(
+                name,
+                {
+                    "first": {"event": "", "time": dstr2date("300101")},
+                    "last": {"event": "", "time": dstr2date("160520")},
+                    "info": gun_info.get(name, dict(name=name, id=0, rank=1)),
+                },
+            )
+            if start_time <= rescue_log[name]["first"]["time"]:
+                rescue_log[name]["first"] = {"event": event, "time": start_time}
+            if end_time >= rescue_log[name]["last"]["time"]:
+                rescue_log[name]["last"] = {"event": event, "time": end_time}
 
 # rescue_log = sorted(rescue_log.values(),key=lambda x:x['info']['id'])
-rescue_log = sorted(rescue_log.values(),key=lambda x:(x['last']['time'] if x['last']['time']!=dstr2date('300101') else datetime.date(2000,1,1),x['first']['time'],x['info']['id']),reverse=True)
+rescue_log = sorted(
+    rescue_log.values(),
+    key=lambda x: (
+        x["last"]["time"]
+        if x["last"]["time"] != dstr2date("300101")
+        else datetime.date(2000, 1, 1),
+        x["first"]["time"],
+        x["info"]["id"],
+    ),
+    reverse=True,
+)
 
 # %%
 
 today = datetime.date.today()
 rows = []
 for record in rescue_log:
-    last_time = record['last']['time']
+    last_time = record["last"]["time"]
     row = dict(
-        id=record['info']['id'],
-        name=record['info']['name'],
-        rank=record['info']['rank'],
-        first_event=record['first']['event'],
-        first_time=record['first']['time'],
-        last_event=record['last']['event'],
-        last_time=record['last']['time'],
-        delta='',
+        id=record["info"]["id"],
+        name=record["info"]["name"],
+        rank=record["info"]["rank"],
+        first_event=record["first"]["event"],
+        first_time=record["first"]["time"],
+        last_event=record["last"]["event"],
+        last_time=record["last"]["time"],
+        delta="",
     )
     rows.append(row)
 df = pd.DataFrame(rows)
 
 table = df.style
 styles = [
-    {'selector': '','props': 'margin-left: auto; margin-right: auto; text-align: center;padding: 0.5em 1em;width:72em'},
-    {'selector': 'th,td','props': 'padding: 0.5em 1em;'},
-    {'selector': 'thead','props': 'background-color: #EEE; color: #333;display: block;'},
-    {'selector': 'tbody','props': 'color: white;display: block;overflow: auto;width: 100%;height: 90vh;'},
-    {'selector': 'tbody tr:nth-child(even)','props': 'background-color: #222'},
-    {'selector': 'tbody tr:nth-child(odd)','props': 'background-color: #444'},
-    {'selector': 'tbody tr:hover','props': 'background-color:#666'},
-    {'selector': 'caption','props':'color:black;text-align: right;'},
-] 
-width = [
-    {'selector': f'td:nth-child({c+1}),th:nth-child({c+1})','props':f'width: {w}em;'} for c,w in enumerate([2,15,3,10,10,10,10,10])
+    {
+        "selector": "",
+        "props": "margin-left: auto; margin-right: auto; text-align: center;padding: 0.5em 1em;width:72em",
+    },
+    {"selector": "th,td", "props": "padding: 0.5em 1em;"},
+    {
+        "selector": "thead",
+        "props": "background-color: #EEE; color: #333;display: block;",
+    },
+    {
+        "selector": "tbody",
+        "props": "color: white;display: block;overflow: auto;width: 100%;height: 90vh;",
+    },
+    {"selector": "tbody tr:nth-child(even)", "props": "background-color: #222"},
+    {"selector": "tbody tr:nth-child(odd)", "props": "background-color: #444"},
+    {"selector": "tbody tr:hover", "props": "background-color:#666"},
+    {"selector": "caption", "props": "color:black;text-align: right;"},
 ]
-table.set_table_styles(styles+width)
+width = [
+    {"selector": f"td:nth-child({c+1}),th:nth-child({c+1})", "props": f"width: {w}em;"}
+    for c, w in enumerate([2, 15, 3, 10, 10, 10, 10, 10])
+]
+table.set_table_styles(styles + width)
 
 table.hide(axis="index")
+
 
 def header_format(key):
     return dict(
@@ -90,11 +121,13 @@ def header_format(key):
         first_time="登场时间",
         last_event="最近打捞活动",
         last_time="结束时间",
-        delta="距今"
+        delta="距今",
     )[key]
-table.format_index(header_format,axis=1)
 
-html_string = '''
+
+table.format_index(header_format, axis=1)
+
+html_string = """
 {table}
 <script>
 var now = new Date().getTime()
@@ -123,10 +156,10 @@ for (let i=0; i < col6s.length; i++) {{
     col7s[i].style.color=day_color
 }}
 </script>
-'''
+"""
 
-with open('rescue.html','w',encoding='utf-8') as f:
+with open("rescue.html", "w", encoding="utf-8") as f:
     f.write(html_string.format(table=table.to_html()))
-df.to_csv('rescue.csv')
+df.to_csv("rescue.csv")
 
 # %%
